5146719d0d636276ae702be7c9b6b98e7b939811
novamd01 <mashaomamtei@gmail.com>
2025-07-05 07:12:18 +0300

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/p0xeh3.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://github.com/novaxmd/BMB-DATA/raw/refs/heads/main/image/allmenu.jpg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
@@ -43,7 +43,7 @@ OWNER_NAME: process.env.OWNER_NAME || "nova-xmd",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝙱.𝙼.𝙱-𝚇𝙼𝙳*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/p0xeh3.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/novaxmd/BMB-DATA/raw/refs/heads/main/image/allmenu.jpg",
 // add img for alive msg
 LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *NOVA-XMD*⚡",
 // add alive msg here 
