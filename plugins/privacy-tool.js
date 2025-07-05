70d62c601284c8960a8504b36b20b1c8ab1f0090
novamd01 <mashaomamtei@gmail.com>
2025-07-05 07:21:26 +0300

    Update privacy-tool.js

--- plugins/privacy-tool.js
+++ plugins/privacy-tool.js
@@ -41,7 +41,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://files.catbox.moe/p0xeh3.jpg` }, // Replace with privacy-themed image if available
+                image: { url: `https://github.com/novaxmd/BMB-DATA/raw/refs/heads/main/image/allmenu.jpg` }, // Replace with privacy-themed image if available
                 caption: privacyMenu,
                 contextInfo: {
                     mentionedJid: [m.sender],
