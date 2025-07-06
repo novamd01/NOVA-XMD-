3f7ed298ec63307c71e6ecb43c4331e83eaab801
novamd01 <mashaomamtei@gmail.com>
2025-07-06 19:36:06 +0300

    Update tag-admins.js

--- plugins/tag-admins.js
+++ plugins/tag-admins.js
@@ -41,7 +41,7 @@ async (conn, mek, m, { from, participants, reply, isGroup, senderNumber, groupAd
             teks += `${randomEmoji} @${admin.split('@')[0]}\n`;
         }
 
-        teks += "└──✪ NEXUS ┃ XMD ✪──";
+        teks += "└──✪ 𝗡𝗢𝗩𝗔 ┃ 𝗫𝗠𝗗 ✪──";
 
         conn.sendMessage(from, { text: teks, mentions: admins }, { quoted: mek });
 
